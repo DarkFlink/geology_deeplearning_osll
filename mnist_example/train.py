a45872cc23cb022c68ac21176f72afd76de927d2
SuperSolik <serpetr99@gmail.com>
2020-03-31 16:26:26 +0300

    #2 added train accuracy, changed hyper parameters

--- mnist_example/train.py
+++ mnist_example/train.py
@@ -8,9 +8,9 @@ from mnist_example.model import MNISTNet
 input_size = 784
 hidden_size = 500
 num_classes = 10
-num_epochs = 30
+num_epochs = 50
 batch_size = 100
-learning_rate = 0.001
+learning_rate = 0.0001
 
 np.random.seed(0)
 
@@ -27,8 +27,8 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 train_data = train_dataset.data.to(device)
 train_labels = train_dataset.targets.to(device)
 
-test_data = train_dataset.data.to(device)
-test_labels = train_dataset.targets.to(device)
+test_data = test_dataset.data.to(device)
+test_labels = test_dataset.targets.to(device)
 
 train_data = train_data.float()
 test_data = test_data.float()
@@ -41,6 +41,7 @@ mnist_net = MNISTNet(input_size, hidden_size, num_classes).to(device)
 loss = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(mnist_net.parameters(), lr=learning_rate)
 
+train_accuracy_history = []
 test_accuracy_history = []
 test_loss_history = []
 
@@ -62,22 +63,33 @@ for epoch in range(num_epochs):
 
         optimizer.step()
 
+    train_outputs = mnist_net.forward(train_data)
     test_outputs = mnist_net.forward(test_data)
+
+    train_accuracy = (train_outputs.argmax(dim=1) == train_labels).float().mean()
+    test_accuracy = (test_outputs.argmax(dim=1) == test_labels).float().mean()
+
+    train_accuracy_history.append(train_accuracy)
+    test_accuracy_history.append(test_accuracy)
+
     test_loss = loss(test_outputs, test_labels)
     test_loss_history.append(test_loss)
-
-    test_accuracy = (test_outputs.argmax(dim=1) == test_labels).float().mean()
-    test_accuracy_history.append(test_accuracy)
 
     print(f'Epoch: {epoch} - Loss: {test_loss:.6f}')
 
 torch.save(mnist_net.state_dict(), "./mnist_model.pth")
 
 plt.figure(1, figsize=(8, 5))
+plt.title("train accuracy")
+plt.plot(train_accuracy_history)
+plt.savefig("./train_accuracy_history.png")
+
+plt.figure(2, figsize=(8, 5))
 plt.title("test accuracy")
 plt.plot(test_accuracy_history)
-plt.savefig("./accuracy_history.png")
-plt.figure(2, figsize=(8, 5))
+plt.savefig("./test_accuracy_history.png")
+
+plt.figure(3, figsize=(8, 5))
 plt.title("test loss history")
 plt.plot(test_loss_history)
-plt.savefig("./loss_history.png")
+plt.savefig("./test_loss_history.png")
