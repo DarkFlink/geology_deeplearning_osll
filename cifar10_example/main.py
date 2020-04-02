4d268e1298c628fce7983762fa00ca90e7e94ca1
SuperSolik <serpetr99@gmail.com>
2020-04-02 13:57:03 +0300

    #3 added vgg11net

---
+++ cifar10_example/main.py
@@ -0,0 +1,91 @@
+import matplotlib.pyplot as plt
+import torch
+import torchvision.datasets
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
+from cifar10_example.model import VGG11Net
+
+num_epochs = 10
+batch_size = 100
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+train_dataset = torchvision.datasets.CIFAR10(root='./data',
+                                             download=True,
+                                             train=True,
+                                             transform=transforms.ToTensor())
+
+test_dataset = torchvision.datasets.CIFAR10(root='./data',
+                                            download=True,
+                                            train=False,
+                                            transform=transforms.ToTensor())
+
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+
+# classes = ('airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+def train(model, train_loader, floss, optimizer, ep_num, train_accuracy_history):
+    model.train()
+    train_correct = 0.
+    total = 0.
+
+    for index, (images, labels) in enumerate(train_loader):
+        optimizer.zero_grad()
+
+        images = images.to(device)
+        labels = labels.to(device)
+
+        outputs = net.forward(images)
+
+        loss_eval = floss(outputs, labels)
+        loss_eval.backward()
+        optimizer.step()
+
+        total += labels.size(0)
+        _, train_predicted = outputs.max(1)
+        train_correct += train_predicted.eq(labels).sum().item()
+
+    train_acc = train_correct / total
+    train_accuracy_history += [train_acc]
+    print(f'Epoch {ep_num} - Train acc {train_acc:.2f}')
+
+
+def test(model, test_loader, ep_num, test_accuracy_history):
+    model.eval()
+    with torch.no_grad():
+        test_correct = 0.
+        total = 0.
+        for index, (images, labels) in enumerate(test_loader):
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = net.forward(images)
+
+            total += labels.size(0)
+            _, test_predicted = outputs.max(1)
+            test_correct += test_predicted.eq(labels).sum().item()
+
+        test_acc = test_correct / total
+        test_accuracy_history += [test_acc]
+        print(f'Epoch {ep_num} - Test acc {test_acc:.2f}')
+
+
+net = VGG11Net().to(device)
+optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+
+loss = torch.nn.CrossEntropyLoss()
+
+test_accuracy_history = []
+train_accuracy_history = []
+
+for epoch in range(num_epochs):
+    train(net, train_loader, loss, optimizer, epoch, train_accuracy_history)
+    test(net, test_loader, epoch, test_accuracy_history)
+
+plt.figure(1, figsize=(10, 8))
+plt.plot(test_accuracy_history, color='blue', label='test')
+plt.plot(train_accuracy_history, color='red', label='train')
+plt.legend(loc='upper left')
---
+++ cifar10_example/model.py
@@ -0,0 +1,56 @@
+import torch.nn as nn
+
+
+class VGG11Net(nn.Module):
+    def __init__(self):
+        super(VGG11Net, self).__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2),
+
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2),
+
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2),
+
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2),
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(512, 512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512, 512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512, 10),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        outputs = self.fc(x)
+        return outputs
