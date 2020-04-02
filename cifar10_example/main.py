8e3d04788390287a78141c771ac8a16397c969b1
SuperSolik <serpetr99@gmail.com>
2020-04-02 15:25:04 +0300

    #3 added graphics

--- cifar10_example/main.py
+++ cifar10_example/main.py
@@ -1,13 +1,13 @@
-import matplotlib.pyplot as plt
 import torch
 import torchvision.datasets
-from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
 from torchvision import transforms
+from torch.utils.data import DataLoader
 
-from cifar10_example.model import VGG11Net
+from cifar10_example.model import Net
 
 num_epochs = 10
-batch_size = 100
+batch_size = 50
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -24,14 +24,13 @@ test_dataset = torchvision.datasets.CIFAR10(root='./data',
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
-# classes = ('airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+classes = ('airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 
 def train(model, train_loader, floss, optimizer, ep_num, train_accuracy_history):
     model.train()
     train_correct = 0.
     total = 0.
-
     for index, (images, labels) in enumerate(train_loader):
         optimizer.zero_grad()
 
@@ -47,10 +46,10 @@ def train(model, train_loader, floss, optimizer, ep_num, train_accuracy_history)
         total += labels.size(0)
         _, train_predicted = outputs.max(1)
         train_correct += train_predicted.eq(labels).sum().item()
-
-    train_acc = train_correct / total
-    train_accuracy_history += [train_acc]
-    print(f'Epoch {ep_num} - Train acc {train_acc:.2f}')
+        if index != 0 and index % 200 == 0:
+            train_acc = train_correct / total
+            train_accuracy_history += [train_acc]
+            print(f'Epoch {ep_num} - Train acc {train_acc:.3f}')
 
 
 def test(model, test_loader, ep_num, test_accuracy_history):
@@ -68,13 +67,14 @@ def test(model, test_loader, ep_num, test_accuracy_history):
             _, test_predicted = outputs.max(1)
             test_correct += test_predicted.eq(labels).sum().item()
 
-        test_acc = test_correct / total
-        test_accuracy_history += [test_acc]
-        print(f'Epoch {ep_num} - Test acc {test_acc:.2f}')
+            if index != 0 and index % 40 == 0:
+                test_acc = test_correct / total
+                test_accuracy_history += [test_acc]
+                print(f'Epoch {ep_num} - Test acc {test_acc:.3f}')
 
 
-net = VGG11Net().to(device)
-optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+net = Net().to(device)
+optimizer = torch.optim.Adam(net.parameters(), lr=0.0003)
 
 loss = torch.nn.CrossEntropyLoss()
 
@@ -89,3 +89,6 @@ plt.figure(1, figsize=(10, 8))
 plt.plot(test_accuracy_history, color='blue', label='test')
 plt.plot(train_accuracy_history, color='red', label='train')
 plt.legend(loc='upper left')
+
+torch.save(net.state_dict(), "./model2.pth")
+plt.savefig("./accuracy2.png")
