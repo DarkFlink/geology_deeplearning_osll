63a02bc6c9986786cefbd05a0f7bae0d0692261b
didges <39410991+didges@users.noreply.github.com>
2020-04-03 14:00:12 +0300

    cifar-10 example

--- 3_poplavskiy_example/main.py
+++ 3_poplavskiy_example/main.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[18]:
-
-
 import torch
 import torchvision
 import torchvision.transforms as transforms
@@ -13,69 +7,80 @@ import matplotlib.pyplot as plt
 import numpy as np
 from torchvision.datasets import CIFAR10
 from torch.utils.data import DataLoader
+from tqdm import tqdm_notebook
+
+n_epoch = 30
+lr = 10e-4
+batch_size = 64
 
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 trainset = CIFAR10(root='./data', train=True, download=True, transform=transform)
-trainloader = DataLoader(trainset, batch_size=128, shuffle=True)
+trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
 
 testset = CIFAR10(root='./data', train=False, download=True, transform=transform)
-testloader = DataLoader(testset, batch_size=128, shuffle=True)
+testloader = DataLoader(testset, batch_size=batch_size, shuffle=False)
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+net = Net().to(device)
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv1 = nn.Conv2d(3, 64, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.conv2 = nn.Conv2d(64, 128, 3)
+        self.fc1 = nn.Linear(128 * 6 * 6, 250)
+        self.fc2 = nn.Linear(250, 115)
+        self.fc3 = nn.Linear(115, 10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
+        
+        x = x.view(-1, 128 * 6 * 6)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
-    
-net = Net()
 
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+optimizer = torch.optim.Adam(net.parameters(), lr=lr)
 
 loss_arr = []
-
-for epoch in range(2):  
-    for i, (inputs, labels) in enumerate(trainloader, 0):
+for epoch in tqdm_notebook(range(n_epoch)):
+    net.train()
+    curr_loss = 0.0
+    i = 0
+    for batch in tqdm_notebook(trainloader):
         optimizer.zero_grad()
+        inputs, target = batch
+        inputs = inputs.to(device)
+        target = target.to(device)
         outputs = net(inputs)
-        loss = criterion(outputs, labels)
+        loss = criterion(outputs, target)
         loss.backward()
         optimizer.step()
-        loss_arr.append(loss.item())
+        curr_loss += loss.item()
+        i+=1
+    print('[%d] loss: %.3f' % (epoch + 1, curr_loss /i))
+    loss_arr.append(curr_loss/i)
+    curr_loss = 0.0
+    
         
-
+net.eval()
 correct = 0
 total = 0
 with torch.no_grad():
     for data in testloader:
         images, labels = data
+        images = images.to(device)
+        labels = labels.to(device)
         outputs = net(images)
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
 
-print('Аккуратность сети на 10000 тестовых изображений: %d %%' % (100 * correct / total))
-
-
-# In[ ]:
-
-
-
-
+print("Точность сети на 10000 тестовых изображений: {}%".format(100 * correct / total))
+plt.plot(range(n_epoch),loss_arr)
